b5c4de5ea1edd9859d2213640a119ac28008099f
showex <official.showex@gmail.com>
2022-05-22 12:15:41 +0400

    FirstCommit

---
+++ src/GameField.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameField extends JPanel {
+
+    public GameField(){
+        setBackground(Color.black);
+    }
+
+}
---
+++ src/MainWindow.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    private final int cellsSize = 16;
+    private final int numberOfCells = 25;
+    private final int size = cellsSize * numberOfCells;
+
+    public MainWindow() {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(size, size);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        MainWindow mw = new MainWindow();
+    }
+}
